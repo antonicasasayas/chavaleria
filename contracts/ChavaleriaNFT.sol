55294f95c63b4127611199560c6fac7797ead7c7
antonicasasayasg <antoni.casasayas@gmail.com>
2023-02-20 12:17:57 +0100

    pepe

---
+++ contracts/ChavaleriaNFT.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ChavaleriaNFT is ERC721URIStorage, Ownable {
+    constructor() payable ERC721("Chavaleria", "CH") {}
+
+    function mint(
+        address _to,
+        uint256 _tokenId,
+        string calldata _uri
+    ) external {
+        _mint(_to, _tokenId);
+        _setTokenURI(_tokenId, _uri);
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
+import { ethers} from "ethers";
+import roboPunksNFT from "./RoboPunksNFT.json";
+const roboPunksNFTAddress = "0x196E863Dc5e888743ae6C3ceE5D9F0C4601F21c3";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [accounts, setAccounts] = useState([]);
+	const isConnected = Boolean(accounts[0]);
+	const [mintAmount, setMintAmount] = useState(1);
+
+	const handleMint = async () => {
+		if (window.ethereum) {
+			const provider = new ethers.providers.Web3Provider(window.ethereum);
+			const signer = provider.getSigner();
+			const contract = new ethers.Contract(
+				roboPunksNFTAddress,
+				roboPunksNFT.abi,
+				signer
+			);
+			try {
+				const response = await contract.mint(
+					accounts[0],
+					1,
+					"ipfs://bafkreicirnoudxeareo375iffa3hkvtyxptau2klc4qk3zxvjukvmv7g54"
+				);
+				console.log("response", response);
+			} catch (err) {
+				console.log(err);
+			}
+		}
+	};
+	const connectAccount = async () => {
+		if (window.ethereum) {
+			const accounts = await window.ethereum.request({
+				method: "eth_requestAccounts",
+			});
+			setAccounts(accounts);
+		}
+	};
+
+	const handleDecrement = async () => {
+		if (mintAmount <= 1) return;
+		setMintAmount((prevAmount) => prevAmount - 1);
+	};
+
+	const handleIncrement = async () => {
+		if (mintAmount >= 3) return;
+		setMintAmount((prevAmount) => prevAmount + 1);
+	};
+	console.log(accounts[0]);
+
+	return (
+		<div className="App">
+			{isConnected ? (
+				<p>Connected</p>
+			) : (
+				<button onClick={connectAccount}>Connect</button>
+			)}
+			{isConnected ? (
+				<div>
+					<button onClick={handleDecrement}>-</button>
+					<input type="number" value={mintAmount} />
+					<button onClick={handleIncrement}>+</button>
+					<button onClick={handleMint}>Mint</button>
+				</div>
+			) : (
+				""
+			)}
+		</div>
+	);
 }
 
 export default App;
